5a396a64e8df4287bfefb59684c8163ae3ba472f
Tilo Himmelsbach <dertilo@gmail.com>
2020-11-02 13:08:52 +0100

    renaming

--- prefix_beam_search.py
+++ prefix_beam_search.py
@@ -36,58 +36,64 @@ def prefix_beam_search(ctc, lm=None, k=25, alpha=0.30, beta=5, prune=0.001):
     Pb, Pnb = defaultdict(Counter), defaultdict(Counter)
     Pb[0][O] = 1
     Pnb[0][O] = 0
-    A_prev = [O]
+    prefixes = [O]
     # END: STEP 1
 
     # STEP 2: Iterations and pruning
     for t in range(1, T):
         pruned_alphabet = [alphabet[i] for i in np.where(ctc[t] > prune)[0]]
-        for l in A_prev:
+        for pref in prefixes:
 
-            if len(l) > 0 and l[-1] == ">":
-                Pb[t][l] = Pb[t - 1][l]
-                Pnb[t][l] = Pnb[t - 1][l]
-                continue
+            is_phrase_end = len(pref) > 0 and pref[-1] == ">"
+            if is_phrase_end:
+                Pb[t][pref] = Pb[t - 1][pref]
+                Pnb[t][pref] = Pnb[t - 1][pref]
+                continue  # goto next prefix
 
             for c in pruned_alphabet:
                 c_ix = alphabet.index(c)
                 # END: STEP 2
 
                 # STEP 3: “Extending” with a blank
+                blank_or_not_prob = Pb[t - 1][pref] + Pnb[t - 1][pref]
                 if c == BLANK:
-                    Pb[t][l] += ctc[t][BLANK_IDX] * (Pb[t - 1][l] + Pnb[t - 1][l])
+                    Pb[t][pref] += ctc[t][BLANK_IDX] * (blank_or_not_prob)
                 # END: STEP 3
 
                 # STEP 4: Extending with the end character
                 else:
-                    l_plus = l + c
-                    if len(l) > 0 and c == l[-1]:
-                        Pnb[t][l_plus] += ctc[t][c_ix] * Pb[t - 1][l]
-                        Pnb[t][l] += ctc[t][c_ix] * Pnb[t - 1][l]
+                    pref_ext = pref + c  # extended prefix
+                    is_same_as_before = len(pref) > 0 and c == pref[-1]
+                    character_prob = ctc[t][c_ix]
+                    token_is_ending = len(pref.replace(" ", "")) > 0 and c in (" ", ">")
+                    if is_same_as_before:
+                        Pnb[t][pref_ext] += character_prob * Pb[t - 1][pref]
+                        Pnb[t][pref] += character_prob * Pnb[t - 1][pref]
                     # END: STEP 4
 
                     # STEP 5: Extending with any other non-blank character and LM constraints
-                    elif len(l.replace(" ", "")) > 0 and c in (" ", ">"):
-                        lm_prob = lm(l_plus.strip(" >")) ** alpha
-                        Pnb[t][l_plus] += (
-                            lm_prob * ctc[t][c_ix] * (Pb[t - 1][l] + Pnb[t - 1][l])
+                    elif token_is_ending:
+                        lm_prob = lm(pref_ext.strip(" >")) ** alpha
+                        Pnb[t][pref_ext] += (
+                            lm_prob * character_prob * (blank_or_not_prob)
                         )
-                    else:
-                        Pnb[t][l_plus] += ctc[t][c_ix] * (Pb[t - 1][l] + Pnb[t - 1][l])
+                    else:  # within token
+                        Pnb[t][pref_ext] += character_prob * (blank_or_not_prob)
                     # END: STEP 5
 
                     # STEP 6: Make use of discarded prefixes
-                    if l_plus not in A_prev:
-                        Pb[t][l_plus] += ctc[t][BLANK_IDX] * (
-                            Pb[t - 1][l_plus] + Pnb[t - 1][l_plus]
+                    if pref_ext not in prefixes:
+                        blank_or_not_pref_ext = (
+                            Pb[t - 1][pref_ext] + Pnb[t - 1][pref_ext]
                         )
-                        Pnb[t][l_plus] += ctc[t][c_ix] * Pnb[t - 1][l_plus]
+                        Pb[t][pref_ext] += ctc[t][BLANK_IDX] * blank_or_not_pref_ext
+                        Pnb[t][pref_ext] += character_prob * Pnb[t - 1][pref_ext]
                     # END: STEP 6
 
         # STEP 7: Select most probable prefixes
         A_next = Pb[t] + Pnb[t]
         sorter = lambda l: A_next[l] * (len(W(l)) + 1) ** beta
-        A_prev = sorted(A_next, key=sorter, reverse=True)[:k]
+        prefixes = sorted(A_next, key=sorter, reverse=True)[:k]
         # END: STEP 7
 
-    return A_prev[0].strip(">")
+    return prefixes[0].strip(">")
